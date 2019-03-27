daa6cb5815440eeded512b00781a533b664c4c0c
klaussunshine <klaussunshine@126.com>
2019-03-27 22:51:39 +0800

    Correct spelling

--- app/src/main/java/com/daniel/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/daniel/broadcastbestpractice/LoginActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
 
-    private EditText mAccout;
+    private EditText mAccount;
     private EditText mPassword;
     private Button mLogin;
 
@@ -18,13 +18,13 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAccout = findViewById(R.id.et_accout);
+        mAccount = findViewById(R.id.et_accout);
         mPassword = findViewById(R.id.et_password);
         mLogin = findViewById(R.id.btn_login);
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String accout = mAccout.getText().toString();
+                String accout = mAccount.getText().toString();
                 String password = mPassword.getText().toString();
                 //如果账号是admin，且密码是123456，就认为登陆成功
                 if (accout.equals("admin") && password.equals("123456")) {
